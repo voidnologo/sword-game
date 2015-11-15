42106e33a0878aabc62be3335228531f9d2c653e
voidnologo <void.no.logo@icloud.com>
2015-11-14 23:03:12 -0600

    monster movement
    tut 13

--- classes.py
+++ classes.py
@@ -1,4 +1,6 @@
+import math
 import pygame
+from random import randint
 
 
 class BaseSprite(pygame.sprite.Sprite):
@@ -66,3 +68,20 @@ class Monster(BaseSprite):
     def __init__(self, x, y, width, height, image_string):
         super(Monster, self).__init__(x, y, width, height, image_string)
         Monster.List.add(self)
+        self.velx = randint(1, 4)
+        self.amplitude = randint(20, 140)
+        self.period = randint(4, 5) / 100.0
+
+    def motion(self, SCREENWIDTH):
+
+        if self.rect.x + self.width > SCREENWIDTH or self.rect.x < 0:
+            self.image = pygame.transform.flip(self.image, True, False)
+            self.velx = -self.velx
+
+        self.rect.x += self.velx
+        self.rect.y = self.amplitude * math.sin(self.period * self.rect.x) + 140
+
+    @staticmethod
+    def movement(SCREENWIDTH):
+        for monster in Monster.List:
+            monster.motion(SCREENWIDTH)
--- main.py
+++ main.py
@@ -11,8 +11,11 @@ clock = pygame.time.Clock()
 FPS = 24
 
 background = pygame.image.load('images/background.png')
-main_character = MainCharacter(0, SCREENHEIGHT - 128, 128, 128, 'images/sword_right.png')
-monster = Monster(40, 100, 90, 50, 'images/monster.png')
+main_character = MainCharacter(0, SCREENHEIGHT - 128, 128, 128, 'images/sword.png')
+monster1 = Monster(40, 100, 90, 50, 'images/monster.png')
+monster2 = Monster(40, 200, 90, 50, 'images/monster.png')
+monster3 = Monster(40, 300, 90, 50, 'images/monster.png')
+monster4 = Monster(40, 400, 90, 50, 'images/monster.png')
 
 while True:
 
@@ -20,6 +23,8 @@ while True:
 
     # <LOGIC>
     main_character.motion(SCREENWIDTH, SCREENHEIGHT)
+    Monster.movement(SCREENWIDTH)
+
     # </LOGIC>
 
     # <DRAW>
