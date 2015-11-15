1fa2ec687c79e17eecedbf46be45bfdba0d8800d
voidnologo <void.no.logo@icloud.com>
2015-11-14 20:07:08 -0600

    loading images
    tut 5

--- main.py
+++ main.py
@@ -1,22 +1,14 @@
 import pygame
 import sys
-import random
 
 
 pygame.init()
-screen = pygame.display.set_mode((640, 360), 0, 32)
+WIDTH, HEIGHT = (640, 360)
+screen = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
 clock = pygame.time.Clock()
-FPS = 60
+FPS = 24
 
-r = 0
-g = 0
-b = 0
-rc = False
-gc = False
-bc = False
-radius = 5
-x = random.randint(1, 639)
-y = random.randint(1, 359)
+img_sword = pygame.image.load('sword.png')
 
 while True:
 
@@ -28,43 +20,13 @@ while True:
     # </PROCESS>
 
     # <LOGIC>
-    if not rc and not gc and not bc:
-        r += 1
-        if r > 255:
-            r = 0
-            rc = True
-
-    if rc and not gc and not bc:
-        g += 1
-
-        if g > 255:
-            g = 0
-            gc = True
-
-    if rc and gc and not bc:
-        b += 1
-
-        if b > 255:
-            b = 0
-            bc = True
-
-    if rc and gc and bc:
-        rc = False
-        gc = False
-        bc = False
-
-    if (x - radius) < 0 or (x + radius) > 640 or (y - radius) < 0 or (y + radius) > 360:
-        x = random.randint(1, 639)
-        y = random.randint(1, 359)
-        radius = 5
-
-    radius += 1
     # </LOGIC>
 
     # <DRAW>
-    screen.fill((r, g, b))
+    x = 200
+    y = 200
 
-    pygame.draw.circle(screen, (b, r, g), (x, y), radius, 5)
+    screen.blit(img_sword, (x, y))
 
     pygame.display.flip()
     # </DRAW>
